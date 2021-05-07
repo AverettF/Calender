f11ee7bf23fb2fe98ec68cceea81536d6ef378e7
FAveret <shuvalov.fedor.2002@mail.ru>
2021-05-07 18:32:51 +0500

    Creat DB?

---
+++ DataAccessLibrary/Models/PersonModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLibrary.Models
+{
+    public class PersonModel
+    {
+        //userid|username|hashPassword|Groupid
+        public string userid { get; set; }
+        public string username { get; set; }
+        public string hashPassword { get; set; }
+        public string Groupid { get; set; }
+    }
+}
---
+++ Database/bdo/Table/Table1.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [userid] UNIQUEIDENTIFIER NULL, 
+    [username] NVARCHAR(50) NULL, 
+    [hashPassword] NVARCHAR(50) NULL, 
+    [Groupid] NVARCHAR(50) NULL
+)
